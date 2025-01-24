8def37e1a6d2557416b465af5ec1db39904344a2
Kaname Sasaki <kaname.sasaki@dlr.de>
2025-01-24 21:26:21 +0100

    Add view factor test case for diff_element_to_ellipse

---
+++ crates/sterad-view-factor/tests/diff_element_to_ellipse_test.rs
@@ -0,0 +1,98 @@
+use std::f64::consts::PI;
+
+fn linspace_middle(start: f64, end: f64, num: usize) -> Vec<f64> {
+    let step = (end - start) / num as f64;
+    let start_middle = start + step / 2.0;
+    (0..num).map(|i| start_middle + i as f64 * step).collect()
+}
+
+/// Calculate the numerical view factor of an ellipse from a differential plate.
+/// - the differential plate is located at (0, 0, 0)
+/// - the ellipse is given by: (x-xc)^2/a^2 + (y-yc)^2/b^2 = 1, z = zc
+///
+/// # Arguments
+///
+/// * `a` - Semi-major axis of the ellipse.
+/// * `b` - Semi-minor axis of the ellipse.
+/// * `theta` - Polar angle of the differential planar element.
+/// * `phi` - Azimuthal angle of the differential planar element.
+/// * `xc` - X-coordinate of the center of the ellipse.
+/// * `yc` - Y-coordinate of the center of the ellipse.
+/// * `zc` - Z-coordinate of the center of the ellipse.
+/// * `a_num` - Number of divisions along the semi-major axis.
+/// * `b_num` - Number of divisions along the semi-minor axis.
+///
+/// # Returns
+///
+/// * `f64` - The numerical view factor of the ellipse from the differential planar element.
+///
+fn diff_element_to_ellipse_numerical(
+    a: f64,
+    b: f64,
+    theta: f64,
+    phi: f64,
+    xc: f64,
+    yc: f64,
+    zc: f64,
+    a_num: usize,
+    b_num: usize,
+) -> f64 {
+    let vec_a = linspace_middle(-a + xc, a + xc, a_num);
+    let vec_b = linspace_middle(-b + yc, b + yc, b_num);
+    let dsize = 4.0 * a * b / (a_num * b_num) as f64;
+    let a2 = a.powi(2);
+    let b2 = b.powi(2);
+
+    // direction of the differential plate
+    let dir_x = theta.sin() * phi.cos();
+    let dir_y = theta.sin() * phi.sin();
+    let dir_z = theta.cos();
+
+    // direction of the mesh element is always (0, 0, -1)
+
+    let mut vf: f64 = 0.0;
+    for x in &vec_a {
+        for y in &vec_b {
+            if (x - xc).powi(2) / a2 + (y - yc).powi(2) / b2 > 1.0 {
+                continue;
+            }
+            let s2 = x.powi(2) + y.powi(2) + zc.powi(2);
+            let s = s2.sqrt(); // distance from the diff_plate to the mesh
+            let plate_to_mesh_x = x / s;
+            let plate_to_mesh_y = y / s;
+            let plate_to_mesh_z = zc / s;
+            let cos_theta_1 =
+                plate_to_mesh_x * dir_x + plate_to_mesh_y * dir_y + plate_to_mesh_z * dir_z;
+            let cos_theta_2 = plate_to_mesh_z;
+            let vf_mesh = dsize * cos_theta_1 * cos_theta_2 / (PI * s2);
+            vf += vf_mesh;
+        }
+    }
+    vf
+}
+
+// cargo test --test diff_element_to_ellipse_test
+#[cfg(test)]
+mod tests {
+    use super::diff_element_to_ellipse_numerical;
+    use std::f64::consts::PI;
+    use sterad_view_factor::diff_element_to_ellipse;
+
+    #[test]
+    fn test_diff_element_to_ellipse() {
+        let a = 2.0;
+        let b = 1.0;
+        let theta = PI / 4.0;
+        let phi = 0.0;
+        let xc = 0.0;
+        let yc = 0.0;
+        let zc = 1.0;
+        let a_num = 10000;
+        let b_num = 10000;
+        let vf_ana = diff_element_to_ellipse::tilted_center(zc, a, b, theta, phi).unwrap();
+        let vf_num = diff_element_to_ellipse_numerical(a, b, theta, phi, xc, yc, zc, a_num, b_num);
+        println!("vf_ana: {}", vf_ana);
+        println!("vf_num: {}", vf_num);
+        assert!((vf_ana - vf_num).abs() < 1e-6);
+    }
+}
